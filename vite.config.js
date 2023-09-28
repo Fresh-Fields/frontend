1166dd825f69f432ee916265acf1c3a4dd8091c1
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2023-09-28 17:39:34 +0530

    config

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path';
+
+export default {
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        landing: resolve(__dirname, 'landing.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        ml: resolve(__dirname, 'ml.html'),
+      },
+    }
+  }
+}
