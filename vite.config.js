c1842f96b86ff25311b0cd7093abfc2957501230
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2024-01-01 14:18:05 +0530

    fixed ridiculous, weird, weird things

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default {
       input: {
         base: resolve(__dirname, 'base.html'),
         landing: resolve(__dirname, 'landing.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        success: resolve(__dirname, 'success.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
         ml: resolve(__dirname, 'ml.html'),
       },
