afd17f64bdc1cc232f5d47ed98ac1b439a20124e
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2024-03-08 08:46:09 +0530

    stashing again

--- src/accounts.js
+++ src/accounts.js
@@ -1,9 +1,33 @@
-export let login = async (data) => fetch("/login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-}).then(res => res.json())
-  .then(data => data)
-  .catch(_ => -1);
+export const login = async (data) => {
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error during login:', error);
+    return Promise.reject(-1);
+  }
+};
+
+export const signup = async (data) => {
+  try {
+    const response = await fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error during signup:', error);
+    return Promise.reject(-1);
+  }
+};
